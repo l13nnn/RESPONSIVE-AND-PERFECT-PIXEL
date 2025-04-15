7024ae9a10361cd2aef6266cc7fb9931065603d4
l13nnn <irenedjaya288@gmail.com>
2025-04-15 11:52:51 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className='background'>
+        <div className='background-1'>
+          <div className='text'>
+            <h2>Kecerdasan visual</h2>
+            <h1>Lihat dunia dari <br /> lensa yang baru.</h1>
+            <p>Gunakan <span className='bold'>kecerdasan visual</span> <br /> melalui Kontrol Kamera untuk <br /> langsung mempelajari tentang <br /> semua hal yang Anda liat. <br /> Cukup <span className='bold'>arahkan iPhone 16 Pro</span> <br /> <span className='bold'>Anda untuk menemukan lebih</span> <br /> atau berinteraksi dengan <br /> sesuatu di depan Anda. Cari <br /> tahu di mana tempat membeli <br /> barang baru yang Anda lihat, <br /> identifikasikan tanaman dan hewan, dan <br /> banyak lagi.</p>
+          </div>
+          <div className='image'>
+            <img src="https://s3-alpha-sig.figma.com/img/2c92/727a/f508b52dbc762450046e9fa820612089?Expires=1745798400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=ChJl0W9S1N5D~28cvJa0PrlotSxK7~t6UgynweuudzzJb-Bsu17I-4ao9QtSPMu~rmbD2uutH91UhYw7SIGaL547NQtTv~86juq4Bz6jcwSCcOhNQ3JwzPsrENT6txE1a7xwpIh7GPOp80laewiHpxidUmmpcdCVQ4cbEnydLTUtewB5NWLGh-4Za3HMPxgEC~kz6bomJw3GJgnHrU6LuvxWsphg4QU9-UABL3khztQ2rf2oyxpVpRG2L7CxzpS17F745-C1C3w-5~IuMp0OJLOFAOcFZYZYT1ANlUCfbpHGcGM5~~7Fg61fGo~6vMBCxqN2lh2-krGiPhafb1sXSg__" />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App;
